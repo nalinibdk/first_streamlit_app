d57755c340d4c6e0844a47dc9422ed31fc06568c
nalinibdk <139764656+nalinibdk@users.noreply.github.com>
2023-07-18 13:13:00 +0530

    Update streamlit_app.py
    text and header added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents healthy dinner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale Spinach')
+streamlit.text('Hard-Boiled')
+
+
