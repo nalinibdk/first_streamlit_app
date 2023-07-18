4fe80a44be6cad3f1e1b8c552f881074b5ecbfef
nalinibdk <139764656+nalinibdk@users.noreply.github.com>
2023-07-18 13:16:36 +0530

    Update streamlit_app.py
    Added breakfast menu with 3 iteams

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My parents healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Eggs')
 
 
 
