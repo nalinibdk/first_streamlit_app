31ee8b014f603450aa5e73a183b17f56788619ce
nalinibdk <139764656+nalinibdk@users.noreply.github.com>
2023-08-09 21:05:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,10 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+my_data_row = my_cur.fetchone()
+streamlit.header("The fruit load list contain:")
+streamlit.dataframe(my_data_row)
+
 
 
 
