9743dce9afd27a2b77fe7dbdfaa69f720c8957ba
nalinibdk <139764656+nalinibdk@users.noreply.github.com>
2023-08-09 22:33:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,6 +96,24 @@ streamlit.write('Thanks for  adding', add_my_fruit)
 #my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit')")
 
 
+#Add remove
+
+def  get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+   # write your own comment -what does the next line do? 
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+         streamlit.error("Please select a fruit to get information. ")
+  else:
+    back_from_function=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+    
+
 
 
 
